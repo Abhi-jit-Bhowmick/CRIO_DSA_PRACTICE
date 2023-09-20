4a9714eca9ad1e78916f250d4ee00bc2f520c4c6
Abhi-jit-Bhowmick <bumba.abhi2012@gmail.com>
2023-09-20 13:26:37 +0530

    Commit on 20-09.2023

---
+++ INTV-1_SPRINT/_1)Find_the_Reverse_Of_The_Number.js
@@ -0,0 +1,21 @@
+/**
+ * ! Questions Link : 
+ *  https://www.crio.do/learn/PSDS/ME_DSA_LEVEL1_PRACTICE1/ME_DSA_LEVEL1_PRACTICE1_MODULE_REVERSENUMBASIC/
+*/
+
+
+/**
+ * @description:: Approach -1::
+ * 
+ * (i) Convert in to string 
+ *          (15) -> "15"    || Time Complexity is O(K)
+ * (ii) Reverse the string
+ *          "15" -> "51"    || Time Complexity is O(K)
+ * (iii) convert string to number
+ *          "51" -> 51      || Time Complexity is O(K)
+ * 
+ * So, the ultimate Time Complexity is   = O(K)+O(K)+O(K)
+ *                                      =O(3K) = O(K)   [Where K is the Number of Input Digit]
+ */
+
+
