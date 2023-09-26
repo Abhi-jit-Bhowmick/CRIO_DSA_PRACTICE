9eedd980a8e80f4ea699b32c74e2dcb469493841
Abhi-jit-Bhowmick <bumba.abhi2012@gmail.com>
2023-09-26 13:54:54 +0530

    Commit on 26-09-2023 at 13:54

---
+++ INTV-1_SPRINT/_011)_Find_The_Two_String_Are_VAlid_Anagram.js
@@ -0,0 +1,17 @@
+/** 
+ * ! Question::
+ * @link :: https://www.crio.do/learn/PSDS/ME_DSA_LEVEL1_PRACTICE1/ME_DSA_LEVEL1_PRACTICE1_MODULE_VALIDANAGRAM/
+*/
+
+/** 
+ * @description:: Approach - 1:
+ * 
+ * (i) 
+ *  
+ * 
+*/
+
+
+function validAnagram(s, t) {
+
+}
---
+++ INTV-1_SPRINT/_012)_Given_String_Is_Valid_Parenthises.js
@@ -0,0 +1,24 @@
+/** 
+ * ! Question ::
+ * @link : https://www.crio.do/learn/PSDS/ME_DSA_LEVEL1_PRACTICE1/ME_DSA_LEVEL1_PRACTICE1_MODULE_VALIDPARENTHESIS/
+*/
+
+
+function validParenthesis(s) {
+
+}
+
+// console.log('_'.charCodeAt(0))
+
+function isAlphaNumeric(char) {
+    console.log(char.charCodeAt(0))
+}
+
+isAlphaNumeric("a")
+isAlphaNumeric("z")
+isAlphaNumeric("A")
+isAlphaNumeric("Z")
+isAlphaNumeric("0")
+isAlphaNumeric("9")
+
+
